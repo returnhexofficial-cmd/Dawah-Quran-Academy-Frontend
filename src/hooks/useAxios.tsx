0550f511dbf23890b8099418df328ff188b8f690
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2026-06-16 22:38:25 +0600

    updated

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  // baseURL: "http://localhost:5000/api/v1",
-  baseURL: "https://quran-academy-backend.onrender.com/api/v1",
+  baseURL: "http://localhost:5000/api/v1",
+  // baseURL: "https://quran-academy-backend.onrender.com/api/v1",
 });
 
 const useAxios = () => {
